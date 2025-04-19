cc90fc90cd91180a3e6528050afaf50be9e86770
YashwantHabib <yashwanthabib567@gmail.com>
2025-04-19 21:52:20 +0530

    Feat, experience page

--- src/app/Experience/page.tsx
+++ src/app/Experience/page.tsx
@@ -1,3 +1,30 @@
+import { experience } from "../_data/experience";
+
 export default function Exp() {
-  return <div>Experience</div>;
+  return (
+    <div className="flex-1 py-20 relative">
+      <p className="text-lg text-stone-100">Experience</p>
+      <p className="text-xs text-stone-500">
+        A collection of work I&apos;ve done.
+      </p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-12">
+        {experience.map((exp) => (
+          <div
+            key={exp.company}
+            className="p-2 my-4 rounded-lg cursor-pointer hover:bg-zinc-900"
+          >
+            <div className="flex items-center gap-4">
+              <span className="text-sm text-stone-100 underline decoration-wavy decoration-stone-600 hover:decoration-stone-400">
+                {exp.company}
+              </span>
+              <div className="flex-1 border-t border-stone-700" />
+              <span className="text-xs text-stone-500 ">{exp.duration}</span>
+            </div>
+            <p className="py-2 text-xs text-stone-400 ">{exp.role}</p>
+            <p className="text-xs text-stone-500">{exp.description} </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
