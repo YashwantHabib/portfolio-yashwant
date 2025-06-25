dcc447c56d39e524dc74c8d577df03e16cb78405
YashwantHabib <yashwanthabib567@gmail.com>
2025-06-25 12:27:26 +0530

    protfolio Update

--- src/app/_components/socials.tsx
+++ src/app/_components/socials.tsx
@@ -7,7 +7,7 @@ export default function Socials() {
     <div className="py-4">
       <div className="flex flex-row justify-center gap-4">
         <a
-          href="https://drive.google.com/file/d/18hEq-JtmI_0EgjLvZX-axJnJstI8e95U/view?usp=sharing"
+          href="https://drive.google.com/file/d/1aaFjJESHDP5zq3zW6JaMBoPzaSXHYT2M/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="Resume"
--- src/app/_data/intro.ts
+++ src/app/_data/intro.ts
@@ -13,6 +13,6 @@ export const intro: Intro = {
     "TypeScript",
     "Tailwind",
     "JavaScript",
-    "MongoDB",
+    "React Native",
   ],
 };
